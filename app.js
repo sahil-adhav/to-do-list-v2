e193c6bce72aa231e036a2e09039a223027c7dfd
Sahil Adhav <sahiladhav@Sahils-MacBook-Pro.local>
2023-02-20 19:26:23 +0530

    MongoDb atlas connection faied

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
+const _ = require("lodash");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -60,7 +60,7 @@ app.get("/", function (req, res) {
 });
 
 app.get("/:customList", (req, res) => {
-  const customListName = req.params.customList;
+  const customListName = _.capitalize(req.params.customList);
 
   List.findOne({name : customListName}, (err, foundList) => {
     if(!err){
@@ -80,8 +80,6 @@ app.get("/:customList", (req, res) => {
       }
     }
   });
-
-
 });
 
 app.post("/", function (req, res) {
@@ -115,3 +113,4 @@ app.get("/about", function (req, res) {
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
+
